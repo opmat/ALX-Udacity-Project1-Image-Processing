c714082240144b6acbabb69353d45d61ca020130
opmat <opmat01@yahoo.com>
2022-12-24 13:14:19 +0100

    test: add test for image conversion
    add 2 test for image conversion

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -10,7 +10,6 @@ describe('basic test suite', () => {
 });
 
 describe('Test GET /view/:imageName', function () {
-
   it('responds with status 200 and image file', (done) => {
     supertest(app)
       .get('/view/fjord')
@@ -19,7 +18,7 @@ describe('Test GET /view/:imageName', function () {
       .expect(200)
       .expect('content-length', '2421874')
       .end(function (err, res) {
-        if (err) throw err; 
+        if (err) throw err;
         return done();
       });
   });
@@ -31,7 +30,29 @@ describe('Test GET /view/:imageName', function () {
       .expect('content-type', /jpeg/)
       .expect(400)
       .end(function (err, res) {
-        if (err) throw err; 
+        if (err) throw err;
+        return done();
+      });
+  });
+
+  it('converts jpg image to png responds with status 200 and png image file', (done) => {
+    supertest(app)
+      .get('/convertImage/fjord/jpg/png')
+      .expect('content-type', /png/)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) throw err;
+        return done();
+      });
+  });
+
+  it('responds with status 500 for invalid image file format', (done) => {
+    supertest(app)
+      .get('/convertImage/fjord/jpg/svg')
+      .expect('content-type', /text/)
+      .expect(500)
+      .end(function (err, res) {
+        if (err) throw err;
         return done();
       });
   });
