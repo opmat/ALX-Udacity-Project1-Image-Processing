d7965b632c8cea6799249f49e00abf745f5ebeaf
opmat <opmat01@yahoo.com>
2022-12-26 13:30:57 +0100

    refactor: code refactoring
    code refactoring to catch error

--- src/utilities/imageProcessor.ts
+++ src/utilities/imageProcessor.ts
@@ -7,21 +7,29 @@ const resizeImage = async (
   sizeOptions: { height?: number; width?: number }
 ): Promise<boolean> => {
   let retValue = false;
+  try {
+    await sharp(srcImage)
+      .resize({ height: sizeOptions.height, width: sizeOptions.width })
+      .toFile(destImage)
+      .then((data) => {
+        // generated
+        logger.info(
+          `resizeImage module completed with ${JSON.stringify(data)}`
+        );
+        retValue = true;
+        return retValue;
+      })
+      .catch((err) => {
+        logger.error(`resizeImage module failed with ${JSON.stringify(err)}`);
+        retValue = false;
+        return retValue;
+      });
+  } catch (err) {
+    logger.error(`resizeImage module threw error with ${JSON.stringify(err)}`);
+    retValue = false;
+    return retValue;
+  }
 
-  await sharp(srcImage)
-    .resize({ height: sizeOptions.height, width: sizeOptions.width })
-    .toFile(destImage)
-    .then((data) => {
-      // generated
-      logger.info(`resizeImage module completed with ${JSON.stringify(data)}`);
-      retValue = true;
-      return retValue;
-    })
-    .catch((err) => {
-      logger.error(`resizeImage module failed with ${JSON.stringify(err)}`);
-      retValue = false;
-      return retValue;
-    });
   return retValue;
 };
 
@@ -31,22 +39,29 @@ const convertImageFormat = async (
   newFormat: sharp.AvailableFormatInfo
 ): Promise<boolean> => {
   let retValue = false;
-
-  await sharp(srcImage)
-    .toFormat(newFormat)
-    .toFile(destImage)
-    .then((data) => {
-      // generated
-      logger.info(`convertImage module completed with ${JSON.stringify(data)}`);
-      retValue = true;
-      return retValue;
-    })
-    .catch((err) => {
-      logger.error(`convertImage module completed with ${JSON.stringify(err)}`);
-      retValue = false;
-      return retValue;
-    });
-  return retValue;
+  try {
+    await sharp(srcImage)
+      .toFormat(newFormat)
+      .toFile(destImage)
+      .then((data) => {
+        // generated
+        logger.info(
+          `convertImage module completed with ${JSON.stringify(data)}`
+        );
+        retValue = true;
+        return retValue;
+      })
+      .catch((err) => {
+        logger.error(`convertImage module failed with ${JSON.stringify(err)}`);
+        retValue = false;
+        return retValue;
+      });
+    return retValue;
+  } catch (err) {
+    logger.error(`convertImage module threw error ${JSON.stringify(err)}`);
+    retValue = false;
+    return retValue;
+  }
 };
 
 export default {
